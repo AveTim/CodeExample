bc4a7c4cd48b6102c85158513fcb734318c814ba
Dzmitry <dzmitry@airmail.cc>
2019-03-06 21:47:51 +0300

    Initial Commit

---
+++ TestLoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestLoginApp
+//
+//  Created by Tim on 3/6/19.
+//  Copyright © 2019 Heads and Hands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
